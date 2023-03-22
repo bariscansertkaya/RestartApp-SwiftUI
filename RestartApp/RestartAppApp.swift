26b76ca180eb8efdc23701a748de7b2b46bdc6ca
Barış Can Sertkaya <bariscansertkaya2@gmail.com>
2023-03-22 22:02:05 +0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Barış Can Sertkaya on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
